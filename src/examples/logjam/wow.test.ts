db43f4676fd34086d36602d48eeac06a2f87b0f3
Shaheer Hashim <shaheerhashim10@gmail.com>
2024-02-28 13:06:49 +0500

    Timezone mocking

---
+++ src/examples/logjam/wow.test.ts
@@ -0,0 +1,29 @@
+import { test, expect, vi } from 'vitest';
+import { log } from './log';
+
+vi.mock('react-redux', (args) => {
+  return {
+    useDispatch() {},
+    useSelector() {},
+  };
+});
+
+test('it spies on the multiply method', () => {
+  const mock = vi.fn((x?: string) => {
+    if (x) {
+      return x.repeat(3);
+    }
+  });
+
+  mock();
+  mock();
+  mock();
+
+  const result = mock('wow');
+  vi.spyOn(console, 'log').mockImplementation(() => {});
+  log('log', 1, 2, 3);
+
+  expect(mock).toHaveBeenLastCalledWith('wow');
+  expect(result).toMatchInlineSnapshot('"wowwowwow"');
+  expect(console.log).toHaveBeenCalledWith(1, 2, 3);
+});
